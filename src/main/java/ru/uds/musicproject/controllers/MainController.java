7bbb4aecd22d018b8ec944918396407ff9366693
Dimut2805 <Dimut2805@yandex.ru>
2020-02-14 14:51:34 +0300

    Очистка от мусора

--- src/main/java/ru/uds/musicproject/controllers/MainController.java
+++ src/main/java/ru/uds/musicproject/controllers/MainController.java
@@ -1,19 +1,8 @@
 package ru.uds.musicproject.controllers;
 
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.uds.musicproject.model.GUI;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainController {
